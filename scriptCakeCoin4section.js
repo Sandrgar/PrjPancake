41932b059f82889c2e9f76ebc9e3a469fe31f069
zeno <giuseppezeno97@gmail.com>
2023-05-16 18:45:13 +0200

    js modified

--- scriptCakeCoin4section.js
+++ scriptCakeCoin4section.js
@@ -1,48 +1,56 @@
-// Calculating Supply
+const container = document.querySelector('.mainBuyCakeContainer');
+container.addEventListener('mouseover', startEvent);
 
-let circSupply = 0;
-const intervallo = setInterval(somma, 5);
+function startEvent() {
 
-function somma() {
-    circSupply += 1234567;
-    document.querySelector(".circSupply").innerHTML = circSupply.toLocaleString();
-    if (circSupply > 200605320) {
-        clearInterval(intervallo);
+    // ------Circulating supply script
+
+    let circSupply = 0;
+    const interval = setInterval(increment, 7);
+
+    function increment() {
+        circSupply += 1234567;
+        document.querySelector(".circSupply").innerText = circSupply.toLocaleString();
+        if (circSupply > 200605320) {
+            clearInterval(interval);
+        }
     }
-}
 
-setInterval(() => {
-    circSupply += 13
-    document.querySelector(".circSupply").innerHTML = circSupply.toLocaleString();
-}, 5000);
-
+    setInterval(() => {
+        circSupply += 13
+        document.querySelector(".circSupply").innerText = circSupply.toLocaleString();
+    }, 5000);
 
 
-// Total supply
 
-let totalSupply = 0;
-const intervallo2 = setInterval(somma2, 5);
+    // ------Total supply script
 
-function somma2() {
-    totalSupply += 1234567;
-    document.querySelector(".totalSupply").innerHTML = totalSupply.toLocaleString();
-    if (totalSupply > 385671342) {
-        clearInterval(intervallo2);
+    let totalSupply = 0;
+    const interval2 = setInterval(increment2, 6);
+
+    function increment2() {
+        totalSupply += 1234567;
+        document.querySelector(".totalSupply").innerText = totalSupply.toLocaleString();
+        if (totalSupply > 385671342) {
+            clearInterval(interval2);
+        }
     }
-}
 
 
 
-// // Burned to date
+    // ------Burned to date script
 
-let burnedToDate = 0;
-const intervallo3 = setInterval(somma3, 5);
+    let burnedToDate = 0;
+    const interval3 = setInterval(increment3, 5);
 
-function somma3() {
-    burnedToDate += 2345678;
-    document.querySelector(".burnedToDate").innerHTML = burnedToDate.toLocaleString();
-    if (burnedToDate > 826186824) {
-        clearInterval(intervallo3);
+    function increment3() {
+        burnedToDate += 2345678;
+        document.querySelector(".burnedToDate").innerText = burnedToDate.toLocaleString();
+        if (burnedToDate > 826186824) {
+            clearInterval(interval3);
+        }
     }
+
+    container.removeEventListener('mouseover', startEvent);
 }
 
