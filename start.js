a2e32b4e72616cd9f89a0fc00e196608d7084ead
Alessandro <al3ss4ndr@hotmail.it>
2023-05-17 17:02:20 +0200

    update html, js css

--- start.js
+++ start.js
@@ -1,6 +1,10 @@
 let slideIndex = 1;
 showSlides(slideIndex);     
  
+setInterval(function() { 
+    slideIndex++;
+    showSlides(slideIndex);
+}, 5000);
 
 function currentSlide(n) {
     showSlides(slideIndex = n);
@@ -11,17 +15,20 @@ function showSlides(n) {
     let slides = document.getElementsByClassName("mySlides");
     let dots = document.getElementsByClassName("carousel-buttons");
 
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+    
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
-        
     }
     
 
     for (i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", ""); 
-        
     }
 
     slides[slideIndex - 1].style.display = "block"; 
     dots[slideIndex - 1].className += " active"; 
+    
 }
