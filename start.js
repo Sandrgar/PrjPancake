20df9cb7168c9ff779a25fb66a25caf3c21e40f2
zeno <giuseppezeno97@gmail.com>
2023-05-20 13:36:59 +0200

    removing notes

--- start.js
+++ start.js
@@ -1,34 +1,34 @@
-// let slideIndex = 1;
-// showSlides(slideIndex);     
+let slideIndex = 1;
+showSlides(slideIndex);     
  
-// // setInterval(function() { 
-// //     slideIndex++;
-// //     showSlides(slideIndex);
-// // }, 5000);
+// setInterval(function() { 
+//     slideIndex++;
+//     showSlides(slideIndex);
+// }, 5000);
 
-// function currentSlide(n) {
-//     showSlides(slideIndex = n);
-// }
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
 
-// function showSlides(n) {
-//     let i;
-//     let slides = document.getElementsByClassName("mySlides");
-//     let dots = document.getElementsByClassName("carousel-buttons");
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("carousel-buttons");
 
-//     if (n > slides.length) {
-//         slideIndex = 1;
-//     }
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
     
-//     for (i = 0; i < slides.length; i++) {
-//         slides[i].style.display = "none";
-//     }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
     
 
-//     for (i = 0; i < dots.length; i++) {
-//         dots[i].className = dots[i].className.replace(" active", ""); 
-//     }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", ""); 
+    }
 
-//     slides[slideIndex - 1].style.display = "block"; 
-//     dots[slideIndex - 1].className += " active"; 
+    slides[slideIndex - 1].style.display = "block"; 
+    dots[slideIndex - 1].className += " active"; 
     
-// }
+}
