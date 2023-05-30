629e157f3701fb2e335afa1bb2fa4e27801060c8
Alessandro <al3ss4ndr@hotmail.it>
2023-05-30 17:20:40 +0200

    js fixed

---
+++ carousel.js
@@ -0,0 +1,35 @@
+let slideIndex = 1;
+showSlides(slideIndex);     
+ 
+// setInterval(function() { 
+//     slideIndex++;
+//     showSlides(slideIndex);
+// }, 5000);
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("carousel-buttons");
+
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+        
+    }
+    
+
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", ""); 
+        
+    }
+
+    slides[slideIndex - 1].style.display = "flex"; 
+    dots[slideIndex - 1].className += " active"; 
+}
